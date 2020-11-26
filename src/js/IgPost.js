a8f0ea0969551b02f2f4598b7878f807703a89e3
MLopez8282 <saltairpix@gmail.com>
2020-11-26 02:19:01 +0000

    practice

---
+++ src/js/IgPost.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+export const IgPost = () => {
+return (
+    <>
+    <div className="card text-center " style="width: 24.8rem; height: 34rem;">
+      <div className="card-header">
+        <div>
+          <p>HTML</p>
+          <p>Rigoberto</p>
+        </div>
+      </div>
+      <img
+        style="width:24.8rem; "
+        className="card-img-top "
+        src="https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1336&q=80"
+        alt="Card image cap"
+      />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+    </>
+)
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { IgPost } from "./IgPost";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +7,8 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<IgPost />
 		</div>
 	);
 }
