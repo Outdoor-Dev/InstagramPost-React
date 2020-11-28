49f00084610e09e3d6963635022393a72731331e
MLopez8282 <saltairpix@gmail.com>
2020-11-28 01:41:07 +0000

    update

--- src/js/IgPost.js
+++ src/js/IgPost.js
@@ -1,37 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const IgPost = () => {
+	const [heart, setHeart] = useState(false);
+
 	return (
 		<>
 			<div
-				className="card text-center "
-				style={{ width: 428, height: 595 }}>
-				<div className="card-header d-flex bd-highlight">
+				className="card text-center rounded-0 "
+				style={{ width: 428, height: 589 }}>
+				<div className="card-header d-flex " style={{ height: 55 }}>
 					<div
-						className="html-icon d-flex justify-content-start mt-1  "
+						className="html-icon d-flex justify-content-start   "
 						style={{
 							width: "50%"
 						}}>
-						<i class="fab fa-html5 " />
-					</div>
-					<div className="title-left d-flex justify-content-start ">
-						<p>
-							<strong>HTML</strong>
-						</p>
+						{" "}
+						<i className="fab fa-html5 " />
+						<div className="title-left d-flex justify-content-start flex-column ml-2 ">
+							<p className="html-title mb-0 ">
+								<strong>HTML5</strong>
+							</p>
 
-						<p>Rigoberto</p>
+							<p className="name-title ">Rigoberto</p>
+						</div>
 					</div>
+
 					<div
 						className="ellipsis ml-auto  bd-highlight d-flex justify-content-end mt-1 "
 						style={{
 							width: "100%"
 						}}>
-						<i className="fas fa-ellipsis-v " />
+						<div className="dropdown">
+							<i
+								className="fas fa-ellipsis-v dropdown "
+								href="#"
+								role="button"
+								id="dropdownMenuLink"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+							/>
+							<div
+								className="dropdown-menu"
+								aria-labelledby="dropdownMenuLink">
+								<a
+									className="dropdown-item text-danger"
+									href="#">
+									Report
+								</a>
+								<a
+									className="dropdown-item text-danger"
+									href="#">
+									Unfollow
+								</a>
+								<a className="dropdown-item" href="#">
+									Go to post
+								</a>
+								<a className="dropdown-item" href="#">
+									Share to...
+								</a>
+								<a className="dropdown-item" href="#">
+									Copy Link
+								</a>
+								<a className="dropdown-item" href="#">
+									Embed
+								</a>
+								<a className="dropdown-item" href="#">
+									Change Background Color
+								</a>
+								<a className="dropdown-item" href="#">
+									Cancel
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 				<img
 					style={{ width: 428, height: 320 }}
-					className="card-img-top "
+					className="card-img-top rounded-0"
 					src="https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1336&q=80"
 					alt="Card image cap"
 				/>
@@ -39,12 +85,16 @@ export const IgPost = () => {
 					<div
 						className="icons-left ml-2 ml-2 pl-1 bd-highlight  d-flex justify-content-start mt-3"
 						style={{ width: "50%" }}>
-						<i className="far fa-heart pr-2" />
+						<i
+							className="far fa-heart pr-2"
+							style={{ fontWeight: heart ? "bold" : "" }}
+							onClick={() => setHeart(!heart)}
+						/>
 						<i className="far fa-comment" />
 						<i className="far fa-paper-plane pl-2" />
 					</div>
 					<div
-						className="icon-right mr-2 mt-3  mr-3  bd-highlight d-flex justify-content-end"
+						className="icon-bookmark mr-2 mt-3  mr-3 pr-1 bd-highlight d-flex justify-content-end"
 						style={{ width: "50%" }}>
 						<i className="far fa-bookmark" />
 					</div>
@@ -57,10 +107,10 @@ export const IgPost = () => {
 					</p>
 					<p className="card-text text-left ">
 						Lorem Ipsum is simply dummy text of the printing and
-						typesetting industry. Lorem Ipsum has been the
-						industry's standard dummy text ever since the 1500s,
-						when an unknown printer took a galley of type and
-						scrambled it to make a type consequat.
+						typesetting industry. Lorem Ipsum has been the industry
+						standard dummy text ever since the 1500s, when an
+						unknown printer took a galley of type and scrambled it
+						to make a type consequat.
 					</p>
 				</div>
 			</div>
