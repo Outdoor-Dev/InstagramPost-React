124450dd3efbb6985fdd4bbd3f016d0d12642e6c
MLopez8282 <saltairpix@gmail.com>
2020-11-29 01:23:48 +0000

    updates

--- src/js/IgPost.js
+++ src/js/IgPost.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const IgPost = () => {
 	const [heart, setHeart] = useState(false);
-
+	//const [brokenheart, setBrokenheart] = useState(false);
+	//const [clickcount, setClickcount] = useState(0);
+	const [bookmark, setBookmark] = useState(false);
+	//const [color, setColor] = useState("");
+	//const randomColors = () => {
+		// Math.floor(Math.random() * 16777215).toString(16);
+		
+	};
 	return (
 		<>
 			{" "}
@@ -10,7 +17,11 @@ export const IgPost = () => {
 				<div className="background-image">
 					<div
 						className="card  rounded-top "
-						style={{ width: 350, height: 588 }}>
+						style={{
+							width: 350,
+							height: 588,
+							backgroundColor: color
+						}}>
 						<div
 							className="card-header d-flex "
 							style={{ height: 55 }}>
@@ -76,9 +87,15 @@ export const IgPost = () => {
 											Embed
 										</a>
 										<hr />
-										<a className="dropdown-item" href="#">
+										<a
+											className="dropdown-item"
+										//	onClick={() => {
+											//	setColor(!color);
+                                        //	}}>
+                                        >
 											Change Background Color
 										</a>
+
 										<hr />
 										<a className="dropdown-item" href="#">
 											Cancel
@@ -96,22 +113,44 @@ export const IgPost = () => {
 						/>
 						<div className="four-icons d-flex bd-highlight mb-3 ">
 							<div
-								className="icons-left ml-2 ml-2 pl-1 bd-highlight  d-flex justify-content-start mt-3"
+								className="icons-left ml-2 pl-1 bd-highlight  d-flex justify-content-start mt-3"
 								style={{ width: "50%" }}>
 								<i
 									className="far fa-heart pr-2 "
+									// {
+									//	heart && brokenheart
+									//	? "fas fa-heart-broken pr-2"
+									//	: "far fa-heart pr-2 "
+									//}
 									style={{
-										fontWeight: heart ? "bold" : ""
+										fontWeight: heart ? "bold" : "",
+										color: heart
+											? "rgb(185, 19, 19)"
+											: "black"
 									}}
-									onClick={() => setHeart(!heart)}
+									onClick={() => {
+										//	if (clickcount === 0) {
+										setHeart(!heart);
+										//	setClickcount(1);
+										//	} else {
+										//	setBrokenheart(!brokenheart);
+										//	}
+									}}
 								/>
-								<i class="far fa-comment" />
+
+								<i className="far fa-comment" />
 								<i className="far fa-paper-plane pl-2" />
 							</div>
 							<div
 								className="icon-bookmark mr-2 mt-3  mr-3 pr-1 bd-highlight d-flex justify-content-end"
 								style={{ width: "50%" }}>
-								<i className="far fa-bookmark" />
+								<i
+									className="far fa-bookmark"
+									style={{
+										fontWeight: bookmark ? "bold" : ""
+									}}
+									onClick={() => setBookmark(!bookmark)}
+								/>
 							</div>
 						</div>
 
